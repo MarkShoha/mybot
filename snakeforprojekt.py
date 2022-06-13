ef26d4316ecdbdebb8f19d6ae74a31006cea456d
MarkShoha <msho7730@gmail.com>
2022-06-13 11:47:48 +0300

    5

--- snakeforprojekt.py
+++ snakeforprojekt.py
@@ -3,18 +3,18 @@ import time
 import random
 pygame.init()
 white = (255, 255, 255)
-yellow = (255, 255, 102)
+yellow = (0, 0, 0)
 black = (0, 0, 0)
 red = (213, 50, 80)
-green = (0, 255, 0)
-blue = (50, 153, 213)
+green = (0, 2, 0)
+blue = (5, 150,0)
 dis_width = 600
 dis_height = 400
 dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('змейка от markOriginalPhoto')
 clock = pygame.time.Clock()
 snake_block = 10
-snake_speed = 8 
+snake_speed = 8
 font_style = pygame.font.SysFont("bahnschrift", 25)
 score_font = pygame.font.SysFont("comicsansms", 35)
 def Your_score(score):
